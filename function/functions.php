a7c4544ea7933e93b21781a7b254c629b1af8129
clementwahaga <c.wahaga@cci-formation.nc>
2021-06-28 09:51:08 +1100

    add function PDO

--- function/functions.php
+++ function/functions.php
@@ -1,1 +1,18 @@
-/*ce fichier contiendra toutes les fonctions*/
+<!-- /*ce fichier contiendra toutes les fonctions*/ -->
+
+<?php
+function pdo_connect_mysql() {
+  $user='root';
+  $pass="";
+    
+  try {
+    $dbh = new PDO('mysql:host=localhost;dbname=blog', $user, $pass);
+    echo "connexion établie" . "<br/>";
+    return $dbh;
+        
+               
+  }catch (PDOException $e) {
+    print "Erreur, vous avez etait deconnecté !: " . $e->getMessage() . "<br/>";
+    return false;
+    }
+}
