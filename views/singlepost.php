11eacae75fa3a74e4a998b6415d6bcf0264f9801
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-06-28 20:09:18 +0200

    path modif

--- views/singlepost.php
+++ views/singlepost.php
@@ -18,4 +18,4 @@ ob_start();
 </form>
 <?php $content = ob_get_clean(); //ici je stocke tout le contenu entre le ob_start et le ob_get_clean dans la variable $content?>
 
-<?php require('./views/template.php'); //ici j'appelle le fichier template.php pour lui envoyer la variable $content?>
+<?php require('template.php'); //ici j'appelle le fichier template.php pour lui envoyer la variable $content?>
