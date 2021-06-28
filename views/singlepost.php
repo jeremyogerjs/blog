fe441183084f6a6e6d36d865bef6181972d42b45
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-06-28 22:56:39 +0200

    modif

--- views/singlepost.php
+++ views/singlepost.php
@@ -5,17 +5,28 @@ ob_start();
 ?>
 
 
+<div>
+    <h1><?= $result['title'] ?></h1>
+    <p><?= $result['content']?></p>
+    <time><?= $result['createDate'] ?></time>
+</div>
 
-<h1><?= $post->title ?></h1>
-<p><?= $post->content ?></p>
-<time><?= $post->createDate ?></time>
+    <hr>
+    <form action="createComm.php?id=<?= $post->id ?>" method="post">
+       
+       <div class="mb-3">
+            <label for="pseudo">Pseudo</label>
+            <input class="form-control" type="text" name="pseudo" id="pseudo"><br><br>
+        </div>
+        <div class="mb-3">
+            <label for="comment">Commentaires</label>
+            <textarea class="form-control" name="comment" id="comment" cols="20" rows="8"></textarea><br><br>
+        </div>
+        <input type="submit" value="envoyé">
+    </form>
+    <hr>
 
-<form action="createComm.php?id=<?= $post->id ?>" method="post">
-<label for="pseudo">Pseudo</label>
-<input type="text" name="pseudo" id="pseudo"><br><br>
-<label for="comment">Commentaires</label>
-<textarea name="comment" id="comment" cols="30" rows="10"></textarea>
-</form>
+
 <?php $content = ob_get_clean(); //ici je stocke tout le contenu entre le ob_start et le ob_get_clean dans la variable $content?>
 
 <?php require('template.php'); //ici j'appelle le fichier template.php pour lui envoyer la variable $content?>
