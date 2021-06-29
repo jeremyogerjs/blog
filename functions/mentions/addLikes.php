e012780b584bcce08b75e62938125cbeeee435a0
Clement Wahaga <c.wahaga@cci-formation.nc>
2021-06-29 11:33:09 +0200

    add  like files

---
+++ functions/mentions/addLikes.php
@@ -0,0 +1,18 @@
+<?php
+require('./helper/db-connect.php');
+
+$bdd = pdo_connect_mysql()
+if(isset($_GET['t'],$_GET['id']){
+
+    $getid = (int) $_GET['id'];
+    $gett = (int) $_GET['t'];
+    $req = $bdd->prepare('SELECT id FROM Posts WHERE id = ?');
+    $req->execute(array($getid));
+    if($req->rowCount() == 1) {
+        if ($gett=0){
+            $checkLike = $bdd->prepare('SELECT id FROM mentions WHERE idPosts = ? ');
+            $checkLike->execute(array($getid)
+        }elseif($gett=1)
+
+    }
+}
