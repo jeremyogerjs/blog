1073e00c74755aa1c1937065f6fc0a85c7ab9d53
Julien Walles <j.walles@cci-formation.nc>
2021-06-29 16:26:35 +1100

    icons

--- views/templateAdmin.php
+++ views/templateAdmin.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
   <title>Blog</title>
@@ -22,7 +23,7 @@
         </li>
         <?php if(empty($_SESSION)) : ?>
         <li class="nav-item">
-          <a class="nav-link" href="index.php?action=admin">Login</a>
+          <a class="nav-link" href="index.php?action=admin"><i class="fas fa-sign-in" aria-hidden="true">Login</i>Login</a>
         </li>
         <?php else : ?>
           <?php endif; ?>
