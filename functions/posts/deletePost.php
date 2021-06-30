c7219fa1dbf7405eaffbe111153623c610ec325f
Julien Walles <j.walles@cci-formation.nc>
2021-06-30 14:48:46 +1100

    delete clear

--- functions/posts/deletePost.php
+++ functions/posts/deletePost.php
@@ -1,15 +1,19 @@
 <?php
+
 $id = is_numeric($_GET['id']) ? htmlspecialchars($_GET['id']) : '';
 
-$sql = "DELETE FROM posts WHERE id = $id";
+$sql = "DELETE FROM posts WHERE posts.id=$id";
+        
 $stmt_del = pdo_connect_mysql()->exec($sql);
+
+
 if($stmt_del > 0) {
 
-	echo '<script type="text/javascript">alert("livre supprimé");
-			window.location.href = "./views/index.php";
+	echo '<script type="text/javascript">alert("post supprimé");
+			window.location.href = "./index.php";
 		  </script>';
 
-    header ('location:index.php');
+    // header ('location:index.php?action=espaceAdmin');
 }
 else {
 	echo '<script>alert("there is an error")</script';
--- views/signInAdmin.php
+++ views/signInAdmin.php
@@ -32,7 +32,6 @@ ob_start();
 	</div>
 </form>
 
-<?php var_dump($_SESSION) ?>
 <?php $content = ob_get_clean(); //ici je stocke tout le contenu entre le ob_start et le ob_get_clean dans la variable $content?>
 
 <?php require('template.php'); //ici j'appelle le fichier template.php pour lui envoyer la variable $content?>
