1fd27a8f7839e466793171aeb6fb1a6cd9887a23
jeremyogerjs <j.oger@cci-formation.nc>
2022-01-04 18:18:48 +1100

    fork

--- views/404.php
+++ views/404.php
@@ -5,7 +5,7 @@ ob_start(); // Demarrage pour chaque fichier views
 
 <p>Cette page que vous recherchez n'existe pas !!!!</p>
 <button class="btn- btn-success"><a href="index.php" class="link-light text-decoration-none">Retour accueil</a></button>
-
+ 
 <?php $content = ob_get_clean(); //ici je stocke tout le contenu entre le ob_start et le ob_get_clean dans la variable $content
 ?>
 
